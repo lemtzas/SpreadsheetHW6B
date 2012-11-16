3835ba6b3d6aa328765a9cf75c4f3538d0273301
lemtzas <lemtzas@gmail.com>
2012-11-15 16:36:07 -0800

    Moving convert function. Fleshing out Cell.

--- src/edu/uw/tcss342/schedule/Cell.java
+++ src/edu/uw/tcss342/schedule/Cell.java
@@ -1,5 +1,7 @@
 package edu.uw.tcss342.schedule;
 
+import java.awt.*;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,8 +18,13 @@ import java.util.Set;
  * To change this template use File | Settings | File Templates.
  */
 public class Cell {
-    public Cell(String formula) {
+    String formula;
+    Set<String> dependencies;
+
+    public Cell(final String formula) {
+        this.formula = formula;
         //calculate dependencies
+        dependencies = new HashSet<String>();
     }
 
     public Double evaluate(Map<String,Double> dep_values) {
@@ -25,8 +32,29 @@ public class Cell {
         return null;
     }
 
+    /**
+     * The cells this cell's value depends on, in set format.
+     * @return Set of Dependencies; non-null; possibly empty
+     */
     public Set<String> getDependencies() {
         //return calculated dependencies
-        return null;
+        return new HashSet<String>();
+    }
+
+
+    public static Point convertString(String identifier) {
+        return new Point();
+    }
+
+    public static String convertPoint(Point p){
+        return convertPoint(p.x,p.y);
+    }
+
+    public static String convertPoint(int x, int y) {
+        //get dependencies
+        //make dep graph
+        //topo sort
+        //evaluate points, pass calculated values
+        return "";
     }
 }
